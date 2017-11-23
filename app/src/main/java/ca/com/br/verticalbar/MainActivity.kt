95cbab5a3200ff321d21cf28a9f67441965cf871
= <carloseduardoa43@gmail.com>
2017-11-23 18:32:09 -0200

    pushando tudo

---
+++ app/src/main/java/ca/com/br/verticalbar/MainActivity.kt
@@ -0,0 +1,52 @@
+package ca.com.br.verticalbar
+
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val maxValue = 4000.00
+        verticalBar1.bind(
+                value = 2000.00,
+                maxValue = maxValue,
+                estimatedValue = maxValue,
+                color = ContextCompat.getColor(this, R.color.blue),
+                overColor = Color.BLUE,
+                icon = R.drawable.ic_add_green_10dp,
+                parentHeight = barContainer.layoutParams.height)
+
+        verticalBar2.bind(
+                value = 1200.0,
+                maxValue = maxValue,
+                estimatedValue = 1000.0,
+                color = ContextCompat.getColor(this, R.color.green),
+                overColor = ContextCompat.getColor(this, R.color.dkgreen),
+                icon = R.drawable.ic_add_green_10dp,
+                parentHeight = barContainer.layoutParams.height)
+
+        verticalBar3.bind(
+                value = 600.0,
+                maxValue = maxValue,
+                estimatedValue = 700.0,
+                color = ContextCompat.getColor(this, R.color.orange),
+                overColor = Color.RED,
+                icon = R.drawable.ic_add_green_10dp,
+                parentHeight = barContainer.layoutParams.height)
+
+        verticalBar4.bind(
+                value = 500.0,
+                maxValue = maxValue,
+                estimatedValue = 0.0,
+                color = ContextCompat.getColor(this, R.color.purple),
+                overColor = Color.RED,
+                icon = R.drawable.ic_add_green_10dp,
+                parentHeight = barContainer.layoutParams.height)
+    }
+}
---
+++ app/src/main/java/ca/com/br/verticalbar/VerticalBarView.kt
@@ -0,0 +1,217 @@
+package ca.com.br.verticalbar
+
+import android.content.Context
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.LayerDrawable
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.ViewCompat
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.vertical_bar_view.view.*
+
+class VerticalBarView : LinearLayout {
+
+    constructor(context: Context?) : super(context)
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    private val barStrokeWidth = 1.dpToPx(context)
+    private val barCornerRadius = 8f
+
+    private var estimatedBarLayer: LayerDrawable? = null
+    private var actualBarLayer: LayerDrawable? = null
+    private var overBarLayer: LayerDrawable? = null
+    private var estimatedBarDrawable: GradientDrawable? = null
+    private var actualBarStroke: GradientDrawable? = null
+    private var actualBarSolid: GradientDrawable? = null
+    private var overBarStroke: GradientDrawable? = null
+    private var overBarSolid: GradientDrawable? = null
+
+    init {
+        View.inflate(context, R.layout.vertical_bar_view, this)
+        initDrawables()
+    }
+
+    fun bind(value: Double,
+             maxValue: Double,
+             estimatedValue: Double,
+             color: Int,
+             overColor: Int,
+             icon: Int, parentHeight: Int) {
+
+        setLabels(icon, estimatedValue, value)
+
+        val currentBarValue = Math.max(value, estimatedValue)
+        val barSize = getBarSize(maxValue = maxValue, value = currentBarValue, parentHeight = parentHeight)
+        val overValue = if (estimatedValue == 0.0) 0.0 else Math.max(value - estimatedValue, 0.0)
+
+        setupEstimatedBar(barSize = barSize, color = color)
+        setupActualValueBar(value = value - overValue,
+                currentBarValue = currentBarValue,
+                barSize = barSize,
+                color = color)
+
+        setupOverBar(overValue, currentBarValue, barSize, overColor)
+        setupBackgrounds()
+
+        redraw()
+    }
+
+    private fun setupOverBar(overValue: Double, currentBarValue: Double, barSize: Int, overColor : Int) {
+
+        val width: Int
+        val actualBarRadii: FloatArray
+        val strokeWidth: Int
+        when {
+            overValue > 0 -> {
+                width = getHeightForValue(
+                        value = overValue,
+                        maxValue = currentBarValue,
+                        height = barSize
+                )
+                actualBarRadii = floatArrayOf(0f, 0f, 0f, 0f, 0f, 0f, 0f, 0f)
+                strokeWidth = 0
+                estimatedBarDrawable?.setStroke(barStrokeWidth, overColor)
+                planningReferenceBar.visibility = View.VISIBLE
+            }
+            else -> {
+                width = 0
+                actualBarRadii = floatArrayOf(barCornerRadius, barCornerRadius, barCornerRadius,
+                        barCornerRadius, 0f, 0f, 0f, 0f)
+                strokeWidth = barStrokeWidth
+                planningReferenceBar.visibility = View.GONE
+            }
+        }
+
+        overBar.layoutParams.height = width
+        actualBarSolid?.cornerRadii = actualBarRadii
+        actualBarStroke?.cornerRadii = actualBarRadii
+        overBarSolid?.setColor(overColor)
+        overBarStroke?.setStroke(strokeWidth, overColor)
+    }
+
+    private fun setupActualValueBar(value: Double, currentBarValue: Double, barSize: Int, color: Int) {
+        val actualHeight = getHeightForValue(
+                value = value,
+                maxValue = currentBarValue,
+                height = barSize
+        )
+        actualBar.layoutParams.height = actualHeight
+        actualBarSolid?.setColor(color)
+        actualBarStroke?.setStroke(barStrokeWidth, color)
+    }
+
+    private fun setupEstimatedBar(barSize : Int, color : Int) {
+        estimatedBar.layoutParams.height = barSize
+        estimatedBarDrawable?.setStroke(barStrokeWidth, color)
+    }
+
+    private fun getBarSize(maxValue: Double, value: Double, parentHeight: Int) : Int {
+        val max = Math.abs(maxValue)
+        val currentValue = Math.abs(value)
+
+        val estimatedValueLabelHeight = getEstimatedValueLabelHeight()
+        val actualValueLabelHeight = getActualValueLabelHeight()
+        val imageReferenceHeight = getImageReferenceHeight()
+
+        val estimatedBarLP = estimatedBar.layoutParams as LinearLayout.LayoutParams
+        val actualValueLP = actualValueLabel.layoutParams as LinearLayout.LayoutParams
+        val imageReferenceLP = imageReference.layoutParams as LinearLayout.LayoutParams
+
+        val availableHeight = (parentHeight -
+                estimatedValueLabelHeight -
+                actualValueLabelHeight -
+                imageReferenceHeight -
+                estimatedBarLP.topMargin -
+                imageReferenceLP.topMargin -
+                actualValueLP.topMargin -
+                container.paddingTop -
+                container.paddingBottom -
+                3.dpToPx(context))
+
+        return getHeightForValue(value = currentValue, maxValue = max, height = availableHeight)
+    }
+
+
+    private fun getImageReferenceHeight() : Int {
+        imageReference.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
+        return imageReference.measuredHeight
+    }
+
+    private fun getActualValueLabelHeight() : Int {
+        actualValueLabel.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
+        return actualValueLabel.measuredHeight
+    }
+
+    private fun getEstimatedValueLabelHeight(): Int {
+        estimatedValueLabel.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED)
+        return estimatedValueLabel.measuredHeight
+    }
+
+    private fun getHeightForValue(value: Double, maxValue: Double, height: Int): Int =
+            ((value * height) / maxValue).toInt()
+
+    private fun setLabels(icon: Int, estimatedValue: Double, value: Double) {
+
+        actualValueLabel.text = value.toString()
+
+        if (estimatedValue > 0) {
+            estimatedValueLabel.text = estimatedValue.toString()
+        } else {
+            estimatedValueLabel.visibility = View.INVISIBLE
+        }
+    }
+
+    private fun initDrawables() {
+        estimatedBarLayer = getEstimatedBarDrawable()
+        actualBarLayer = getActualBarDrawable()
+        overBarLayer = getOverBarDrawable()
+
+        estimatedBarDrawable = estimatedBarLayer?.findDrawableByLayerId(R.id.stroke) as GradientDrawable
+        actualBarStroke = actualBarLayer?.findDrawableByLayerId(R.id.strokeRect) as GradientDrawable
+        actualBarSolid = actualBarLayer?.findDrawableByLayerId(R.id.solidRect) as GradientDrawable
+        overBarStroke = overBarLayer?.findDrawableByLayerId(R.id.strokeRect) as GradientDrawable
+        overBarSolid = overBarLayer?.findDrawableByLayerId(R.id.solidRect) as GradientDrawable
+    }
+
+
+    private fun getActualBarDrawable() = ContextCompat.getDrawable(context,
+            R.drawable.vertical_bar_fill_drawable).mutate() as LayerDrawable
+
+    private fun getEstimatedBarDrawable() = ContextCompat.getDrawable(context,
+            R.drawable.vertical_bar_stroke_drawable).mutate() as LayerDrawable
+
+    private fun getOverBarDrawable() = ContextCompat.getDrawable(context,
+            R.drawable.over_vertical_bar_fill_drawable).mutate() as LayerDrawable
+
+    fun linearScale(domain: Pair<Double, Double>, range: Pair<Double, Double>): (Double) -> Double {
+        return { value ->
+            range.first + (range.second - range.first) * ((value - domain.first) / (domain.second - domain.first))
+        }
+    }
+
+    private fun setupBackgrounds() {
+        estimatedBarLayer?.setDrawableByLayerId(R.id.stroke, estimatedBarDrawable)
+        actualBarLayer?.setDrawableByLayerId(R.id.stroke, actualBarStroke)
+        actualBarLayer?.setDrawableByLayerId(R.id.solidRect, actualBarSolid)
+        overBarLayer?.setDrawableByLayerId(R.id.stroke, overBarStroke)
+        overBarLayer?.setDrawableByLayerId(R.id.solidRect, overBarSolid)
+
+        ViewCompat.setBackground(estimatedBar, estimatedBarLayer)
+        ViewCompat.setBackground(actualBar, actualBarLayer)
+        ViewCompat.setBackground(overBar, overBarLayer)
+    }
+
+    private fun redraw() {
+        estimatedBar.invalidate()
+        estimatedBar.requestLayout()
+        actualBar.invalidate()
+        actualBar.requestLayout()
+        overBar.invalidate()
+        overBar.requestLayout()
+        invalidate()
+        requestLayout()
+    }
+
+}
